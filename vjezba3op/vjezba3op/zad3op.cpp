089bfa04379e2df14e8630c9e7e95374b64a4d91
kristiankiki <148130415+kristiankiki@users.noreply.github.com>
2023-11-20 20:19:03 +0100

    Update zad3op.cpp

--- vjezba3op/vjezba3op/zad3op.cpp
+++ vjezba3op/vjezba3op/zad3op.cpp
@@ -5,23 +5,23 @@
 int main() {
 	std::vector<int> vec = { 5, 8, 2, 10, 3, 1, 6 };
 
-	// Sortiraj vektor pomoću STL funkcije sort
+	
 	std::sort(vec.begin(), vec.end());
 
-	// Pronađi najmanji element
+	
 	int minElement = *std::min_element(vec.begin(), vec.end());
 
-	// Pronađi sve najmanje elemente i umetni nulu ispred svakog od njih
+	
 	auto it = vec.begin();
 	while ((it = std::find(it, vec.end(), minElement)) != vec.end()) {
 		vec.insert(it, 0);
 		it = std::next(it, 2); // Pomakni pokazivač za dva koraka
 	}
 
-	// Pronađi najveći element
+	
 	int maxElement = *std::max_element(vec.begin(), vec.end());
 
-	// Izračunaj sumu svih elemenata iza najvećeg elementa
+	
 	int sum = 0;
 	bool afterMax = false;
 
@@ -35,7 +35,7 @@ int main() {
 		}
 	}
 
-	// Ispiši rezultate
+	
 	std::cout << "Sortirani vektor sa 0 ispred najmanjih elemenata:" << std::endl;
 	for (int element : vec) {
 		std::cout << element << " ";
